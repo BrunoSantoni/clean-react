b2f4377297e6000fa2a0df70ce629c432e3c1776
BrunoSantoni <brunosantoni98@gmail.com>
2021-07-25 02:35:02 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,16 @@
 import {
   HttpPostClient,
   HttpPostParams,
-} from 'data/protocols/http/http-post-client';
+} from '../../data/protocols/http/http-post-client';
 
 // Spy captura valores e coloca valores fake
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
+  body?: Record<string, unknown>;
 
   async post(params: HttpPostParams): Promise<void> {
     this.url = params.url;
+    this.body = params.body;
     return Promise.resolve();
   }
 }
