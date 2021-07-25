3dff35975fed6eaabe58d684affc413c2ab822c5
BrunoSantoni <brunosantoni98@gmail.com>
2021-07-25 04:27:45 -0300

    refactor: add generics to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,15 +4,20 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { mockAuthentication } from '@/domain/test/mock-authentication';
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models/account-model';
 import faker from 'faker';
 
 type SutTypes = {
   sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>;
 };
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AuthenticationParams,
+    AccountModel
+  >();
 
   const sut = new RemoteAuthentication(url, httpPostClientSpy); //sut (system under test)
 
