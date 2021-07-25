1612205dce376c291aec6b779ad66488889eeed0
BrunoSantoni <brunosantoni98@gmail.com>
2021-07-25 02:15:58 -0300

    refactor: change httppostclient to receive an object

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,14 @@
-import { HttpPostClient } from 'data/protocols/http/http-post-client';
+import {
+  HttpPostClient,
+  HttpPostParams,
+} from 'data/protocols/http/http-post-client';
 
 // Spy captura valores e coloca valores fake
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
 
-  async post(url: string): Promise<void> {
-    this.url = url;
+  async post(params: HttpPostParams): Promise<void> {
+    this.url = params.url;
     return Promise.resolve();
   }
 }
